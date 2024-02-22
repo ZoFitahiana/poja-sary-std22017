d842f1c48b52c4107583c2ac946a128ced5050a6
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-02-22 10:12:34 +0100

    fix : Resolve format.sh

--- src/main/java/school/hei/sary/endpoint/rest/controller/health/ImageProcessingController.java
+++ src/main/java/school/hei/sary/endpoint/rest/controller/health/ImageProcessingController.java
@@ -8,20 +8,20 @@ import school.hei.sary.service.event.ImageProcessingService;
 @RestController
 @RequestMapping("/api/images")
 public class ImageProcessingController {
-    private final ImageProcessingService imageProcessingService;
+  private final ImageProcessingService imageProcessingService;
 
-    public ImageProcessingController(ImageProcessingService imageProcessingService) {
-        this.imageProcessingService = imageProcessingService;
-    }
+  public ImageProcessingController(ImageProcessingService imageProcessingService) {
+    this.imageProcessingService = imageProcessingService;
+  }
 
-    @PutMapping(value = "/process-black-and-white/{key}", produces = MediaType.IMAGE_PNG_VALUE)
-    public ResponseEntity<byte[]> processToBlackAndWhite(@PathVariable String key, @RequestBody byte[] image) {
-        byte[] processedImage = imageProcessingService.processToBlackAndWhite(image, key);
-        if (processedImage.length > 0) {
-            return ResponseEntity.ok().body(processedImage);
-        } else {
-            return ResponseEntity.badRequest().build();
-        }
+  @PutMapping(value = "/process-black-and-white/{key}", produces = MediaType.IMAGE_PNG_VALUE)
+  public ResponseEntity<byte[]> processToBlackAndWhite(
+      @PathVariable String key, @RequestBody byte[] image) {
+    byte[] processedImage = imageProcessingService.processToBlackAndWhite(image, key);
+    if (processedImage.length > 0) {
+      return ResponseEntity.ok().body(processedImage);
+    } else {
+      return ResponseEntity.badRequest().build();
     }
+  }
 }
-
--- src/main/java/school/hei/sary/service/event/ImageProcessingService.java
+++ src/main/java/school/hei/sary/service/event/ImageProcessingService.java
@@ -13,27 +13,28 @@ import org.springframework.stereotype.Service;
 @NoArgsConstructor
 public class ImageProcessingService {
 
-    public byte[] processToBlackAndWhite(byte[] inputImage, String key) {
-        try {
-            BufferedImage image = ImageIO.read(new ByteArrayInputStream(inputImage));
-            if (image == null) {
-                System.out.println("Impossible de lire l'image. Assurez-vous qu'il s'agit d'une image valide");
-                return new byte[0];
-            }
+  public byte[] processToBlackAndWhite(byte[] inputImage, String key) {
+    try {
+      BufferedImage image = ImageIO.read(new ByteArrayInputStream(inputImage));
+      if (image == null) {
+        System.out.println(
+            "Impossible de lire l'image. Assurez-vous qu'il s'agit d'une image valide");
+        return new byte[0];
+      }
 
-            BufferedImage convertedImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
-            Graphics2D g = convertedImage.createGraphics();
-            g.drawImage(image, 0, 0, null);
-            g.dispose();
+      BufferedImage convertedImage =
+          new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+      Graphics2D g = convertedImage.createGraphics();
+      g.drawImage(image, 0, 0, null);
+      g.dispose();
 
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            ImageIO.write(convertedImage, "png", byteArrayOutputStream);
-            return byteArrayOutputStream.toByteArray();
-        } catch (IOException e) {
-            System.out.println("Impossible de lire l'image. Assurez-vous qu'il s'agit d'une image valide");
-            return new byte[0];
-        }
+      ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+      ImageIO.write(convertedImage, "png", byteArrayOutputStream);
+      return byteArrayOutputStream.toByteArray();
+    } catch (IOException e) {
+      System.out.println(
+          "Impossible de lire l'image. Assurez-vous qu'il s'agit d'une image valide");
+      return new byte[0];
     }
+  }
 }
-
-
